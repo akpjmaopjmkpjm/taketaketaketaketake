6fba8256686f5adcc38406dbe787093734844148
gigigigigi2001 <saanasa883@gmail.com>
2026-07-31 16:33:48 +0900

    28kaime

--- s3_transfer.ps1
+++ s3_transfer.ps1
@@ -3,12 +3,12 @@ $env:AWS_USE_FIPS_ENDPOINT="false"
 
 Write-Output "--- [Start] S3 Transfer Process ---"
 
-# 🔴 `--no-progress` をつけることで、画面のフリーズを完全に防止します
-& "C:\Program Files\Amazon\AWSCLIV2\aws.exe" s3 cp "s3://20260616-resi/deploy.zip" "s3://20260616-send/deploy.zip" --no-progress
+# 🟢 元のシンプルな直接実行に戻します
+& "C:\Program Files\Amazon\AWSCLIV2\aws.exe" s3 cp "s3://20260616-resi/deploy.zip" "s3://20260616-send/deploy.zip"
 
 if ($LASTEXITCODE -ne 0) {
     Write-Output "🚨 AWS CLI failed with exit code: $LASTEXITCODE"
     exit 1
 }
 
-Write-Output "--- [End] S3 Transfer Process Success ---11"
+Write-Output "--- [End] S3 Transfer Process Success ---"
