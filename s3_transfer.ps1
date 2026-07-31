fff7d60b7faa7fc84110e12b6a791d2f419551ef
gigigigigi2001 <saanasa883@gmail.com>
2026-07-31 10:16:36 +0900

    21kaime

--- s3_transfer.ps1
+++ s3_transfer.ps1
@@ -7,4 +7,4 @@ Write-Output "--- 20260616-send への運び出し処理を開始します ---"
 # 💡 仮置き場（resi）から本命（send）へ直接コピーして運びます
 & "C:\Program Files\Amazon\AWSCLIV2\aws.exe" s3 cp "s3://20260616-resi/deploy.zip" "s3://20260616-send/deploy.zip" 2>&1
 
-Write-Output "--- 運び出しが正常に完了しました ---"
+Write-Output "--- 運び出しが正常に完了しました -----"
