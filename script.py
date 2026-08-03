bd71f912394e27c9d8e5aaa1b93da272a44fea8f
gigigigigi2001 <saanasa883@gmail.com>
2026-08-03 18:21:18 +0900

    Lam1kaime

---
+++ script.py
@@ -0,0 +1,10 @@
+import json
+
+def lambda_handler(event, context):
+    # ここに本当に行いたい処理を書きます
+    print("Hello from Lambda!")
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
